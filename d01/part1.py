47d9fabae93ec692bf7885473069fd6590e4cbcd
Muhammad Hamza Farrukh <hamzafarrukh141@gmail.com>
2022-11-30 23:25:16 -0600

    Day 1 - Too slow

---
+++ d01/part1.py
@@ -0,0 +1,31 @@
+import math
+import re
+import heapq
+import sys
+from collections import defaultdict
+from os.path import dirname, abspath, join
+
+
+def readFile(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+
+d = dirname(dirname(abspath(__file__)))
+
+lines = readFile(join(dirname(abspath(__file__)), 'input.txt'))
+
+cur = 0
+ans = 0
+for l in lines:
+    if not l:
+        if cur > ans:
+            ans = cur
+        cur = 0
+    else:
+        cal = int(l)
+        cur += cal
+print(max(ans, cur))
---
+++ d01/part2.py
@@ -0,0 +1,32 @@
+import math
+import re
+import heapq
+import sys
+from collections import defaultdict
+from os.path import dirname, abspath, join
+
+
+def readFile(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+
+d = dirname(dirname(abspath(__file__)))
+
+lines = readFile(join(dirname(abspath(__file__)), 'input.txt'))
+
+cur = 0
+ans = []
+for l in lines:
+    if not l:
+        ans.append(cur)
+        cur = 0
+    else:
+        cal = int(l)
+        cur += cal
+if l:
+    ans.append(cur)
+print(sum(sorted(ans)[-3:]))
