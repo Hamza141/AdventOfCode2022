a8477994bf8eeb0e5c39fcbf2e3adf37416563a6
Muhammad Hamza Farrukh <hamzafarrukh141@gmail.com>
2022-12-05 23:23:07 -0600

    Day 6 - Not bad

---
+++ d06/part2.py
@@ -0,0 +1,31 @@
+import math
+import re
+import heapq
+import sys
+from collections import defaultdict, deque
+from os.path import dirname, abspath, join
+
+
+def readFile(filename):
+    lines = []
+    with open(filename, 'r') as f:
+        for line in f:
+            lines.append(line.strip())
+    return lines
+
+
+lines = readFile(join(dirname(abspath(__file__)), 'input.txt'))
+
+d = deque()
+res = 1
+
+for l in lines:
+    for c in l:
+        while c in d:
+            d.popleft()
+        d.append(c)
+        if len(d) == 14:
+            break
+        res += 1
+
+print(res)
